0a71aff44877f730ab95827362e205c99166efde
Cheng H. Lee <clee@anaconda.com>
2020-06-29 20:20:50 -0500

    Update to pango 1.45.3

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,44 +7,62 @@ fi
 # Use a sledgehammer to avoid libtool `.la` files when linking; this must be
 # done because various packages  we depend on (e.g., libxml2) no longer have
 # libtool `.la` files within them.
-rm -f ${PREFIX}/lib/libcairo*.la
-rm -f ${PREFIX}/lib/libfontconfig*.la
-rm -f ${PREFIX}/lib/libfreetype*.la
-rm -f ${PREFIX}/lib/libharfbuzz*.la
-rm -f ${PREFIX}/lib/libpixman*.la
+find "${PREFIX}/lib" -type f -name "*.la" -delete
+
+# The "fribidi" recipe moves shared libraries out of `lib64` into `lib`, but
+# some builds do not properly account for this in their pkg-config files.
+sed -i.bak "s:/lib64:/lib:" "${PREFIX}/lib/pkgconfig/fribidi.pc"
 
 # We must avoid very long shebangs here.
 echo '#!/usr/bin/env bash' > g-ir-scanner
-echo "${PYTHON} ${PREFIX}/bin/g-ir-scanner \$*" >> g-ir-scanner
+echo "${PREFIX}/bin/python ${PREFIX}/bin/g-ir-scanner \$*" >> g-ir-scanner
 chmod +x ./g-ir-scanner
 export PATH=${PWD}:${PATH}
 
-./configure --prefix="${PREFIX}" \
-            --host=${HOST} \
-            --with-xft \
-            --with-cairo="${PREFIX}"
+mkdir -p builddir
 
-make -j${CPU_COUNT} ${VERBOSE_AT}
-# # FIXME: There is one failure:
-# ========================================
-#    pango 1.40.1: tests/test-suite.log
-# ========================================
-#
-# # TOTAL: 12
-# # PASS:  11
-# # SKIP:  0
-# # XFAIL: 0
-# # FAIL:  1
-# # XPASS: 0
-# # ERROR: 0
-#
-# .. contents:: :depth: 2
-#
-# FAIL: test-layout
-# =================
-#
-# /layout/valid-1.markup:
-# (/opt/conda/conda-bld/work/pango-1.40.1/tests/.libs/lt-test-layout:5078): Pango-CRITICAL **: pango_font_describe: assertion 'font != NULL' failed
-# FAIL test-layout (exit status: 133)
-# make check
-make install
+declare -a configure_extra_opts
+case "${target_platform}" in
+    linux-*)
+        configure_extra_opts+=(-Dintrospection=true)
+        ;;
+    osx-64)
+        # For now, turn off gobject introspection to avoid a build-time link
+        # error (missing "__cg_png_create_info_struct" symbol referenced from
+        # the ImageIO Framework system library).
+        configure_extra_opts+=(-Dintrospection=false)
+
+        # Use conda- (not Apple XCode-provided) object & library manipulation
+        # tools; not doing so will cause the build to fail with "malformed
+        # object" & "load command size is zero" errors when building on systems
+        # with older releases of Xcode (e.g., Xcode 7 on OS X 10.10).
+        ln -s "${INSTALL_NAME_TOOL}" "${BUILD_PREFIX}/bin/install_name_tool"
+        ln -s "${NM}" "${BUILD_PREFIX}/bin/nm"
+        ln -s "${OTOOL}" "${BUILD_PREFIX}/bin/otool"
+        hash -r
+        ;;
+esac
+
+meson setup \
+    --prefix="${PREFIX}" \
+    --libdir="${PREFIX}/lib" \
+    --wrap-mode="nofallback" \
+    --buildtype="release" \
+    --backend=ninja \
+    -Duse_fontconfig=true \
+    ${configure_extra_opts[@]} \
+    builddir
+
+# Print build configuration results
+meson configure builddir
+
+ninja -C builddir -j ${CPU_COUNT} -v
+
+# Requires third-party font (Cantarell), so turn off for now
+#ninja -C builddir -j ${CPU_COUNT} test
+
+ninja -C builddir -j ${CPU_COUNT} install
+
+# Remove any new Libtool files we may have installed. It is intended that
+# conda-build will eventually do this automatically.
+find "${PREFIX}/lib" -name "*.la" -delete
